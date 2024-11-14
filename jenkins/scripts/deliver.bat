ab6b4cf8b3d71dafbc63ee7d7640248eca56ec20
vickrant231 <129058196+vickrant231@users.noreply.github.com>
2024-11-15 00:08:40 +0530

    Update deliver.bat

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -1,77 +1,77 @@
-@echo off
+//@echo off
 
-echo The following Maven command installs your Maven-built Java application
+//echo The following Maven command installs your Maven-built Java application
 
-echo into the local Maven repository, which will ultimately be stored in
+//echo into the local Maven repository, which will ultimately be stored in
 
-echo Jenkins's local Maven repository (and the "maven-repository" Docker data
+//echo Jenkins's local Maven repository (and the "maven-repository" Docker data
 
-echo volume).
+//echo volume).
 
-rem Enable command echoing
+//rem Enable command echoing
 
-setlocal enabledelayedexpansion
+//setlocal enabledelayedexpansion
 
-rem Install your Maven jar and evaluate the project name
-
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
-
-rem Extract the value of the  element from pom.xml
-
-set OUTPUT_NAME=mvn -q -DforceStdout help:evaluate -Dexpression=project.name
-
-for /f "delims=" %%i in ('%OUTPUT_NAME%') do set NAME=%%i
-
-echo The name of the project is: !NAME!
-
-rem Extract the value of the  element from pom.xml
-
-set OUTPUT_VERSION=mvn -q -DforceStdout help:evaluate -Dexpression=project.version
-
-for /f "delims=" %%i in ('%OUTPUT_VERSION%') do set VERSION=%%i
-
-echo The version of the project is: !VERSION!
-
-echo The following command runs and outputs the execution of your Java
-
-echo application (which Jenkins built using Maven) to the Jenkins UI.
-
-java -jar target!NAME!-!VERSION!.jar@echo off
-
-echo The following Maven command installs your Maven-built Java application
-
-echo into the local Maven repository, which will ultimately be stored in
-
-echo Jenkins's local Maven repository (and the "maven-repository" Docker data
-
-echo volume).
-
-rem Enable command echoing
-
-setlocal enabledelayedexpansion
-
-rem Install your Maven jar and evaluate the project name
+//rem Install your Maven jar and evaluate the project name
 
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 
-rem Extract the value of the  element from pom.xml
+//rem Extract the value of the  element from pom.xml
 
-set OUTPUT_NAME=mvn -q -DforceStdout help:evaluate -Dexpression=project.name
+//set OUTPUT_NAME=mvn -q -DforceStdout help:evaluate -Dexpression=project.name
 
-for /f "delims=" %%i in ('%OUTPUT_NAME%') do set NAME=%%i
+//for /f "delims=" %%i in ('%OUTPUT_NAME%') do set NAME=%%i
 
-echo The name of the project is: !NAME!
+//echo The name of the project is: !NAME!
 
-rem Extract the value of the  element from pom.xml
+//rem Extract the value of the  element from pom.xml
 
-set OUTPUT_VERSION=mvn -q -DforceStdout help:evaluate -Dexpression=project.version
+//set OUTPUT_VERSION=mvn -q -DforceStdout help:evaluate -Dexpression=project.version
 
-for /f "delims=" %%i in ('%OUTPUT_VERSION%') do set VERSION=%%i
+//for /f "delims=" %%i in ('%OUTPUT_VERSION%') do set VERSION=%%i
 
-echo The version of the project is: !VERSION!
+//echo The version of the project is: !VERSION!
 
-echo The following command runs and outputs the execution of your Java
+//echo The following command runs and outputs the execution of your Java
 
-echo application (which Jenkins built using Maven) to the Jenkins UI.
+//echo application (which Jenkins built using Maven) to the Jenkins UI.
 
-java -jar target!NAME!-!VERSION!.jar
+//java -jar target!NAME!-!VERSION!.jar@echo off
+
+//echo The following Maven command installs your Maven-built Java application
+
+//echo into the local Maven repository, which will ultimately be stored in
+
+//echo Jenkins's local Maven repository (and the "maven-repository" Docker data
+
+//echo volume).
+
+//rem Enable command echoing
+
+//setlocal enabledelayedexpansion
+
+//rem Install your Maven jar and evaluate the project name
+
+mvn jar:jar install:install help:evaluate -Dexpression=project.name
+
+//rem Extract the value of the  element from pom.xml
+
+//set OUTPUT_NAME=mvn -q -DforceStdout help:evaluate -Dexpression=project.name
+
+//for /f "delims=" %%i in ('%OUTPUT_NAME%') do set NAME=%%i
+
+//echo The name of the project is: !NAME!
+
+//rem Extract the value of the  element from pom.xml
+
+//set OUTPUT_VERSION=mvn -q -DforceStdout help:evaluate -Dexpression=project.version
+
+//for /f "delims=" %%i in ('%OUTPUT_VERSION%') do set VERSION=%%i
+
+//echo The version of the project is: !VERSION!
+
+//echo The following command runs and outputs the execution of your Java
+
+//echo application (which Jenkins built using Maven) to the Jenkins UI.
+
+java -jar maven-app.jar
